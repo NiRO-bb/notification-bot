812f51cda47ff98fdf6f6a4f645640ad65faea99
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-01-26 13:18:03 +0400

    Create project

---
+++ src/test/java/com/github/notification_bot/bot/PetBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.notification_bot.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
